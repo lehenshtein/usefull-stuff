c7d5156e5cbe64cf0bcffbfef2adc6c95b87b401
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-04-26 10:27:06 +0200

    feat(auth): add register, check if user is authenticated, add simple layout for testing auth which will be removed

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth,
+  User,
+  authState,
+  createUserWithEmailAndPassword,
+} from '@angular/fire/auth';
+import { BehaviorSubject, Observable, from, map } from 'rxjs';
+import { IAuthCredentials } from '../models/auth-credentials.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
+  private userSubject = new BehaviorSubject<User | null>(null);
+  user$ = this.userSubject.asObservable();
+
+  constructor(private auth: Auth) {}
+
+  checkIfUserIsAuthenticated() {
+    authState(this.auth).subscribe((user) => {
+      this.userSubject.next(user);
+    });
+  }
+
+  register({ email, password }: IAuthCredentials): Observable<User> {
+    return from(
+      createUserWithEmailAndPassword(this.auth, email, password)
+    ).pipe(map((credentials) => credentials.user));
+  }
 }
