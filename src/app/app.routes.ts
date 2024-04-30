aa2bd72b088fa9b08b57b1b7e73c98630bada33f
lehenshtein <lehenshtein@gmail.com>
2024-04-30 22:55:17 +0300

    Timezones helper (#7)
    * feat: added timezone helper
    
    * feat: body main color reset

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TimezonesComponent } from '@app/pages/timezones/timezones.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'timezones',
+    component: TimezonesComponent
+  }
+];
