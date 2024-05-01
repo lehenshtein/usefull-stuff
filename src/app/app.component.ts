9d3b6656b98415390c58fc55faf5f1c9ed8220ab
Vladyslav Mezhevyi <vlad.mezhevyi@gmail.com>
2024-05-01 21:27:38 +0200

    feat(token): logout when token is expired

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
@@ -6,6 +6,7 @@ import { MenuItem } from 'primeng/api';
 import { InputTextModule } from 'primeng/inputtext';
 import { ToastModule } from 'primeng/toast';
 import { items } from '@shared/helpers/menu-items.helper';
+import { AuthService } from './shared/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -20,6 +21,12 @@ import { items } from '@shared/helpers/menu-items.helper';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private authService = inject(AuthService);
+
   items: MenuItem[] = items;
+
+  ngOnInit(): void {
+    this.authService.checkIfTokenIsExpired();
+  }
 }
